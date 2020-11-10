c309b93b25f8780c8e2c11af285724e25fe7ebcc
kiefergallant <44451951+kiefergallant@users.noreply.github.com>
2020-11-09 18:42:26 -0700

    trying again

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import { Container,Button,Form,FormInput,Col,Row } from "shards-react";
+ 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+ 
 function App() {
+  //Setup To do list 
+  const [toDoList,setTodoList] = useState([
+    {text:"Mow the lawn"},
+    {text:"Wash the dishes"},
+    {text:"Study"}
+  ]);
+
+  const [value,setValue] = useState('');
+  // handle submission of form 
+  const handleSubmit = (mouseClick) => {
+    mouseClick.preventDefault();
+    addToDo(value);
+    setValue('')
+
+  };
+ 
+    // Add to do 
+
+  const addToDo = (text) => {
+    const updatedToDoList = [...toDoList,{text}];
+    setTodoList(updatedToDoList)
+  };
+
+  // Delete to do 
+  const handleDelete = (todo) =>{
+    const filteredTodoList = toDoList.filter(currentTodoListValue => (currentTodoListValue !== todo));
+    setTodoList(filteredTodoList)
+  };
+  
+
+  // 
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Container>
+      <h1>Kiefer's ToDo App</h1>
+      
+      <Container  className = 'todoInput'>
+        <form onSubmit={handleSubmit}>
+          <FormInput
+          id = "todoinput"
+          value = {value}
+          placeholder ='Enter Todo'
+          onChange = {keyBoardStroke => setValue( keyBoardStroke.target.value)}/>
+          <Button id='addtodo'type='submit'>Add To Do</Button>
+        </form>
+      </Container>
+      
+      <Container className = 'todoList'>
+        <Row>
+        <Col id='todocolumn'>
+        
+        {
+        toDoList.map((todo,index) => (
+          <div id = "tododiv" key={index}>
+            <span><p id="todotext">{todo.text}</p></span>
+            <Button id='delete' onClick = {()=>handleDelete(todo)}>Delete</Button>
+            
+          </div>
+          
+        ))}
+        </Col>
+        <Col></Col>
+        </Row>
+      </Container>
+
+      
+   
+   </Container>
+    
   );
-}
+        }
 
 export default App;
